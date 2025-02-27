e60144b48bc25b97f60ef1acf8c9a686b0a8732b
cbroomew <chad.webster@yahoo.com>
2025-02-27 15:51:09 -0500

    created Candidate interface

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+export interface Candidate {
+    login: string,
+    name: string,
+    email: string,
+    bio: string,
+    company: string,
+    location: string,
+    avatar_url: string,
+}
